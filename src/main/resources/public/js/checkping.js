a6e4caa7642f138af2b6e2efad33f1d25ce5d21b
Mitchell van Ek <mitchell.v.ek@xs4all.nl>
2016-12-15 12:05:41 +0100

    hi

--- src/main/resources/public/js/checkping.js
+++ src/main/resources/public/js/checkping.js
@@ -1,4 +1,4 @@
-var clientSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/server");
+var clientSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/ping");
 webSocket.onmessage = function (msg) { ;
 webSocket.onclose = function () { alert("WebSocket connection closed") };
 webSocket.onconnect = function() {clientSocket.send("ping!")}
