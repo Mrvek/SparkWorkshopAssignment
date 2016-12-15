f30ec4da402ef26c3c60f7743a7c65f102507f21
Mitchell van Ek <mitchell.v.ek@xs4all.nl>
2016-12-15 08:55:54 +0100

    set ip

--- src/main/resources/public/js/Medium.js
+++ src/main/resources/public/js/Medium.js
@@ -2,7 +2,7 @@ var clientSocket = new WebSocket("ws://" + location.hostname + ":" + location.po
 webSocket.onmessage = function (msg) { SendToServer(msg) };
 webSocket.onclose = function () { alert("WebSocket connection closed") };
 
-var serverSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat");
+var serverSocket = new WebSocket("ws://145.89.102.114:4567/chat");
 webSocket.onmessage = function (msg) { SendToClient(msg) };
 webSocket.onclose = function () { alert("WebSocket connection closed") };
 
